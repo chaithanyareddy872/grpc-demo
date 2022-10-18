a42ebfb3c8cda4c0e89ed554f2bb595cf4bbda44
suriya.s <suriya.s@globallogic.com>
2022-10-18 08:57:36 +0530

    Added server initializer class

---
+++ recommendation-service/src/main/java/com/musicmantra/recommendationservice/GrpcInitializer.java
@@ -0,0 +1,32 @@
+package com.musicmantra.recommendationservice;
+
+import com.musicmantra.recommendationservice.implementation.RecommendTeacherServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class GrpcInitializer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //Loading application properties file
+        InputStream file= new FileInputStream("recommendation-service/src/main/resources/application.properties");
+
+        Properties properties=new Properties();
+
+        properties.load(file);
+
+        //Building GRPC server
+        Server server= ServerBuilder.forPort(Integer.valueOf(properties.getProperty("server.port"))).addService(new RecommendTeacherServiceImpl()).build();
+
+        server.start();
+
+        System.out.println("GRPC Server started @ "+server.getPort());
+
+        server.awaitTermination();
+    }
+}
